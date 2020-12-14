32b0c930546df4e871fe4496c7ef699d8ac6af53
bishax <alex.bishop@nesta.org.uk>
2020-12-14 17:06:33 +0000

    Moved google_trends to flows/ ; added gtab fork ; deleted dev helpers
    Main TODO is to deal with `gtab_fork.py` which is not production ready at all

--- sg_covid_impact/flows/google_trends/run.py
+++ sg_covid_impact/flows/google_trends/run.py
@@ -4,6 +4,10 @@ from pathlib import Path
 from json import dumps
 from sg_covid_impact import config
 from sg_covid_impact.utils.metaflow import update_model_config, execute_flow
+from sg_covid_impact.google_trends.get_search_trends import (
+    load_salient_words,
+    extract_salient_words,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -13,15 +17,12 @@ if __name__ == "__main__":
     google_config = config["flows"]["google_search"]
     params = google_config["params"]
     anchor_periods = list(params["anchor_periods"].values())
-    with open("terms.txt", "r") as f:
-        terms = f.read().split("\n")
+    terms = load_salient_words().pipe(extract_salient_words, params["threshold"])
 
     cmd_params = {
         "--anchor_periods": dumps(anchor_periods),
         "--terms": dumps(terms),
         "--test_mode": str(params["test_mode"]),
-        "--with": "batch:queue=job-queue-many-nesta-metaflow",
-        # "--max-workers": "256"
     }
     flow_file = Path(__file__).resolve().parents[0] / "gtab_flow_linear.py"
     run_id = execute_flow(
@@ -31,6 +32,9 @@ if __name__ == "__main__":
             "--package-suffixes": ".py,.txt",
             # "--metadata": "local",
             # "--datastore": "local"
+            # "--with": "batch:queue=job-queue-many-nesta-metaflow",
+            # "--max-workers": "256"
+            "--tag sg_covid_impact"
         },
     )
 
--- sg_covid_impact/google_trends/resume.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python gtab_flow_linear.py --no-pylint --package-suffixes .txt,.py resume --with batch:queue=job-queue-many-nesta-metaflow
