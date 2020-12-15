79fbda41d5ec2abf8efb332457d369f5bcbe0091
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2020-12-15 14:04:01 +0000

    Addressing PR comments

--- sg_covid_impact/covid_data.py
+++ sg_covid_impact/covid_data.py
@@ -1,4 +1,4 @@
-import re
+"""Fetch, save, and load Covid-19 case, death, and testing data from data.gov.uk."""
 import requests
 import pandas as pd
 import logging
@@ -6,12 +6,9 @@ import sg_covid_impact
 
 project_dir = sg_covid_impact.project_dir
 
-_BASE_URL = (
-    "https://api.coronavirus.data.gov.uk/v2/data?areaType=_GEO_&_METRICS_&format=json"
-)
+_BASE_URL = "https://api.coronavirus.data.gov.uk/v2/data?areaType={geo}&{metrics}_&format=json".format
 _GEO = "overview"
-_METRICS = ["newCasesByPublishDate", 
-"newDeaths28DaysByDeathDate","newVirusTests"]
+_METRICS = ["newCasesByPublishDate", "newDeaths28DaysByDeathDate", "newVirusTests"]
 _COVID_FILE = f"{project_dir}/data/processed/covid_metrics.csv"
 
 
@@ -29,26 +26,23 @@ def fetch_covid(metrics, geo):
     for q in metrics:
         mq.append(f"metric={q}")
 
-    # Replace metrics placeholder with metric string
-    query_url = re.sub("_METRICS_", "&".join(mq), _BASE_URL)
-    # Replace geo placeholder with geo
-    query_url = re.sub("_GEO_", geo, query_url)
+    # Fill the query
+    query_url = _BASE_URL(geo="overview", metrics="&".join(mq))
 
     # Fetch and parse
     response = requests.get(query_url)
-    df = (pd.DataFrame(response.json()["body"])
-          .melt(
-                id_vars=["date", "areaType", "areaCode", "areaName"],
-                var_name="variable",
-                value_name="value"
-                )
-          )
+    df = pd.DataFrame(response.json()["body"]).melt(
+        id_vars=["date", "areaType", "areaCode", "areaName"],
+        var_name="variable",
+        value_name="value",
+    )
 
     df["date"] = pd.to_datetime(df["date"])
     return df
 
 
 def read_covid():
+    """Read parsed covid table"""
     return pd.read_csv(_COVID_FILE, parse_dates=["date"])
 
 
