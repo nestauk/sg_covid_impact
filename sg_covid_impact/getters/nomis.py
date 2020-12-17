a4ebc1e2e33650bfba0ae893b2144a3e41ee5e72
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2020-12-17 13:37:44 +0000

    Added filelock to env yaml and changed nomis getter

--- sg_covid_impact/getters/nomis.py
+++ sg_covid_impact/getters/nomis.py
@@ -2,7 +2,8 @@
 from sg_covid_impact.utils.metaflow import flow_getter, cache_getter_fn
 import sg_covid_impact
 import pandas as pd
-
+from metaflow import namespace
+namespace(None)
 
 def run_id() -> int:
     """Get `run_id` for flow
