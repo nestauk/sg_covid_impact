90b556660de5d5b94800684a3e3f6aab33577fc3
bishax <alex.bishop@nesta.org.uk>
2020-12-18 09:39:34 +0000

    NSPL data getter

--- sg_covid_impact/getters/nspl.py
+++ sg_covid_impact/getters/nspl.py
@@ -1,2 +1,19 @@
 # %%
-# TODO
+"""Data getter (Research DAPS) for National Statistics Postcode Lookup (NSPL)."""
+import pandas as pd
+
+from sg_covid_impact.utils.metaflow import flow_getter, cache_getter_fn
+import sg_covid_impact
+
+
+def run_id() -> int:
+    """Get `run_id` for flow
+
+    NOTE: This is loaded from __init__.py not from file
+    """
+    return sg_covid_impact.config["flows"]["nspl"]["run_id"]
+
+
+@cache_getter_fn
+def get_nspl() -> pd.DataFrame:
+    return flow_getter("NSPL", run_id=run_id()).nspl_data
