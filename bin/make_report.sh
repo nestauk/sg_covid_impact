8ec1bc08964938a423f4e7f22674207158e0b24a
bishax <alex.bishop@nesta.org.uk>
2021-02-08 12:22:31 +0000

    Fix pdf report generation.

--- bin/make_report.sh
+++ bin/make_report.sh
@@ -5,10 +5,10 @@ cd $(dirname $0)/../reports/technical_report
 pandoc -s 1_introduction.md 2_literature.md 3_methodology.md 4_results.md 5_conclusions.md\
  -f markdown\
  -o report.tex\
- --natbib -F pandoc-crossref\
  --bibliography 'technicalreport.bib'\
  --filter ../../bin/altair_pandoc_filter.py\
- --metadata figure_path="../../figures/scotland"
+ --metadata figure_path="../../figures"\
+ --natbib -F pandoc-crossref
 pdflatex report.tex
 bibtex report || echo "Bibtex unsuccessful!"
 pdflatex report.tex
